83a4cfa088b3c11dc3b456966c75667f432bcd41
M Fauzaan Naashir <mfauzaannsshir@gmail.com>
2026-04-22 10:35:05 +0700

    Delete Try9_oop (1).py

--- Try9_oop (1).py
+++
@@ -1,8 +0,0 @@
-a=8
-b=7
-print("a & b =", a & b, bin(a & b))
-print("a | b =", a | b, bin(a | b))
-print("a ^ b =", a ^ b, bin(a ^ b))
-print("~a =", ~a, bin(~a))
-print("a << 1 =", a << 1, bin(a << 1))
-print("a >> 1 =", a >> 1, bin(a >> 1))
