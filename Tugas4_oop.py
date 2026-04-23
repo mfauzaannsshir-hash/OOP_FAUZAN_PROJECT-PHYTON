4ac48d6925a97894cdc6009c22e7d07237c6485c
M Fauzaan Naashir <mfauzaannsshir@gmail.com>
2026-04-23 09:12:30 +0700

    Add files via upload

---
+++ Tugas4_oop.py
@@ -0,0 +1,15 @@
+
+nama=input("masukan nama:")
+print(nama)
+
+umur=input("masukan umur:")
+print(umur)
+
+no_hp=input("masukan no hp:")
+print(no_hp)
+
+email=input("masukan email:")
+print(email)
+
+alamat=input("masukan alamat:")
+print(alamat)
